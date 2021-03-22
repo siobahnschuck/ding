5464ef49d96cde38e07b204ff3b064ee33c5d0af
siobahnschuck <sschuc@saic.edu>
2021-03-22 14:56:41 -0500

    seeded

--- seeders/20210322170856-user.js
+++ seeders/20210322170856-user.js
@@ -1,20 +1,22 @@
 'use strict'
 const bcrypt = require('bcrypt')
-const { query } = require('express')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.insertOne('user', {
-      firstName: 'Bob',
-      lastName: 'Chef',
-      username: 'bbchef',
-      email: 'bobchef@yahoo.com',
-      passwordDigest: await bcrypt.hash('1234', 1),
-      createdAt: new Date(),
-      updatedAt: new Date()
-    })
+    const users = [
+      {
+        firstName: 'Bob',
+        lastName: 'Chef',
+        username: 'bbchef',
+        email: 'bobchef@yahoo.com',
+        passwordDigest: await bcrypt.hash('1234', 1),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]
+    await queryInterface.bulkInsert('users', users)
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.deleteOne('user')
+    await queryInterface.bulkDelete('users')
   }
 }
