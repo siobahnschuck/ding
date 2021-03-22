4302a501b835d17819fb361bafd2cef999de4d13
siobahnschuck <sschuc@saic.edu>
2021-03-22 13:59:08 -0500

    user seed set

--- seeders/20210322170856-user.js
+++ seeders/20210322170856-user.js
@@ -1,24 +1,20 @@
-'use strict';
-
+'use strict'
+const bcrypt = require('bcrypt')
+const { query } = require('express')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.insertOne('user', {
+      firstName: 'Bob',
+      lastName: 'Chef',
+      username: 'bbchef',
+      email: 'bobchef@yahoo.com',
+      passwordDigest: await bcrypt.hash('1234', 1),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.deleteOne('user')
   }
-};
+}
