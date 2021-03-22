f0a6b64cbc83fe9a29fe7c7e71209366fd6d0bc3
siobahnschuck <sschuc@saic.edu>
2021-03-22 13:48:04 -0500

    recipe seeder

--- seeders/20210322170921-recipe.js
+++ seeders/20210322170921-recipe.js
@@ -1,24 +1,28 @@
-'use strict';
-
+;('use strict')
+const faker = require('faker')
+const { User } = require('../models')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    const users = await User.findAll({ raw: true })
+    let recipes = [...Array(20)].map(() => ({
+      userId: users[Math.floor(Math.random() * users.length)].id,
+      name: faker.lorem.word(),
+      cuisineType: faker.lorem.word(),
+      instructions: faker.lorem.sentence(),
+      image: faker.image.image(),
+      isVegan: faker.random.boolean(),
+      isDairyFree: faker.random.boolean(),
+      hasNuts: faker.random.boolean(),
+      duration: Math.floor(Math.random() * 180),
+      calories: Math.floor(Math.random() * 800),
+      like: faker.random.number(),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }))
+    await queryInterface.bulkInsert('recipes', recipes)
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('recipes')
   }
-};
+}
