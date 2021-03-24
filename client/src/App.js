c20ca66adb2ffc3eb66f75e04a8433acb98661f4
Andrew Liu <liu.andrew1988@gmail.com>
2021-03-24 18:33:46 -0400

    login function boiler plate setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
-import React from 'react'
+
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import About from './pages/About'
 import MyRecipePage from './pages/MyRecipePage'
 import { Route, NavLink, Switch } from 'react-router-dom'
 import './css/App.css'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [authenticated, setAuthenticated] = useState(false)
+
+    const logOut = () => {
+    setAuthenticated(false)
+    localStorage.clear()
+  }
+
   return (
     <div>
       <Switch>
-        <Route exact path="/" render={(props) => <Home {...props} />} />
+        <Route exact path="/" render={(props) => <Home {...props} authenticated={authenticated} setAuthenticated={setAuthenticated}/>} logOut={logOut} />
         <Route path="/dashboard" render={(props) => <Dashboard {...props} />} />
         <Route path="/about" render={(props) => <About {...props} />} />
         <Route
--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -2,11 +2,39 @@ import React, { useState } from 'react'
 import { Modal, Button } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
 import '../css/App.css'
+import axios from 'axios'
+import {BASE_URL} from '../globals'
 
-const SignIn = () => {
+const SignIn = (props) => {
+  console.log(props)
   const [show, setShow] = useState(false)
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
+
+    const [loginForm, handleLoginForm] = useState({
+    username: '',
+    password: ''
+    })
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const res = await axios.post(`${BASE_URL}/auth/login`, loginForm)
+      localStorage.setItem('token', res.data.token)
+      props.setAuthenticated(true)
+      setShow(false)
+      handleLoginForm({ username: '', password: '' })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    handleLoginForm({ ...loginForm, [name]: value })
+  }
+
+
   return (
     <div>
       <Button variant="primary" onClick={handleShow}>
@@ -17,11 +45,24 @@ const SignIn = () => {
           <Modal.Title>Sign In</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <form>
+          <form onSubmit={handleSubmit}>
             <p>username:</p>
-            <input></input>
+            <input
+              type="username"
+              name="username"
+              placeholder="USERNAME"
+              value={loginForm.username}
+              onChange={handleChange}
+              required
+              />
             <p>password:</p>
-            <input></input>
+            <input  
+              type="password"
+              name="password"
+              placeholder="PASSWORD"
+              value={loginForm.password}
+              onChange={handleChange}
+              required/>
           </form>
         </Modal.Body>
         <Modal.Footer>
@@ -29,9 +70,17 @@ const SignIn = () => {
             Close
           </Button>
           <NavLink to="/dashboard">
-            <Button variant="primary" onClick={handleClose}>
+            <button 
+            type ='submit'
+            variant="primary"  
+              disabled={!loginForm.username || !loginForm.password}
+              size="large"
+              color="teal"
+              animated="fade"
+              onClick={handleSubmit}
+              >
               Sign In
-            </Button>
+            </button>
           </NavLink>
         </Modal.Footer>
       </Modal>
