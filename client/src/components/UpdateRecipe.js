c66ef7255c2fb42b5919586343cfd7cd79c00822
Andrew Liu <liu.andrew1988@gmail.com>
2021-03-23 23:42:45 -0400

    update for isVegan, isDairyFree, and hasNuts

--- client/src/components/UpdateRecipe.js
+++ client/src/components/UpdateRecipe.js
@@ -16,7 +16,9 @@ const Edit = (props) => {
     instructions: recipe.instructions, 
     duration: recipe.duration, 
     calories: recipe.calories,
-    isVegan: recipe.isVegan
+    isVegan: recipe.isVegan,
+    isDairyFree: recipe.isDairyFree,
+    hasNuts: recipe.hasNuts
     })
 
 
@@ -38,6 +40,29 @@ const Edit = (props) => {
 
   const handleChange = ({target}) => {
     setUpdate({...update, [target.name]:target.value});
+  }
+
+
+   const handleVeganChange = () => {
+    if (update.isVegan === true) {
+      setUpdate({...update, isVegan: false})
+    } else {
+      setUpdate({...update, isVegan: true})
+    }
+  }
+  const handleDairyChange = () => {
+    if (update.isDairyFree === true) {
+      setUpdate({...update,isDairyFree: false})
+    } else {
+      setUpdate({...update,isDairyFree: true})
+    }
+  }
+  const handleNutsChange = () => {
+    if (update.hasNuts === true) {
+      setUpdate({...update, hasNuts: false})
+    } else {
+      setUpdate({...update, hasNuts:true})
+    }
   }
 
 
@@ -65,9 +90,9 @@ const Edit = (props) => {
             <p>Image:</p>
             <input
               name="image"
-      
-              // value={props.newRecipe.image}
-              // onChange={props.handleChange}
+              placeholder = {recipe.image}
+              value={update.image}
+              onChange={handleChange}
             />
             <p>Title:</p>
             <input
@@ -104,24 +129,31 @@ const Edit = (props) => {
               value={update.calories}
               onChange={handleChange}
             />
+            
             <p>Vegan:</p>
-            <input
+            <div className={update.isVegan ? 'check' : ''}>
+            <input 
               type="checkbox"
-              value={props.isVegan}
-              onClick={props.handleVeganChange}
+              value={update.isVegan}
+              onClick={handleVeganChange}
             />
+            </div>
             <p>DairyFree:</p>
+           <div className={update.isDairyFree ? 'check' : ''}>
             <input
               type="checkbox"
-              value={props.isDairyFree}
-              onClick={props.handleDairyChange}
+              value={update.isDairyFree}
+              onClick={handleDairyChange}
             />
+            </div>
             <p>No Nuts:</p>
+            <div className={update.hasNuts ? 'check' : ''}>
             <input
               type="checkbox"
-              value={props.hasNuts}
-              onClick={props.handleNutsChange}
+              value={update.hasNuts}
+              onClick={handleNutsChange}
             />
+            </div>
             <br/>
             <button type = 'submit'>Submit</button>
           </form>
