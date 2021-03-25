9d866c55188e5d99dc62801ee2f188aa20c12679
queeniewilliams <queeniepu329@gmail.com>
2021-03-25 08:04:43 -0700

    added pantry

---
+++ client/src/components/Pantry.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const pantryList = (props) => {
+  const AddToPantry = () => {
+    console.log(props.name)
+    props.dispatch({
+      type: 'add_pantry',
+      payload: props.ingredient
+    })
+  }
+  return (
+    <div className="ingredientList">
+      {/* <img alt="item" src={props.ingredient.image} /> */}
+      <span>
+        <p>{props.ingredient ? props.ingredient.name : null}</p>
+        <button onClick={() => AddToPantry(props.ingredient)}>+</button>
+      </span>
+    </div>
+  )
+}
+export default pantryList
