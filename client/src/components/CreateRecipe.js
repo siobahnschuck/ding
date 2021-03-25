6d50f4d52246e782c35661d07c8ebfe140b66430
siobahnschuck <sschuc@saic.edu>
2021-03-25 13:09:45 -0500

    clean code up

--- client/src/components/CreateRecipe.js
+++ client/src/components/CreateRecipe.js
@@ -1,48 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Modal, Button } from 'react-bootstrap'
-import Fridge from '../components/Fridge'
-import IngredientList from '../components/IngredientList'
-import axios from 'axios'
 import DetailsRecipeCreate from '../components/DetailsRecipeCreate'
-// import Switch from 'react-switch'
 
 const CreateRecipe = (props) => {
   const [show, setShow] = useState(false)
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
-
-  // useEffect(() => {
-  //   getMyIngredients()
-  // }, [])
-
-  // const getMyIngredients = async () => {
-  //   try {
-  //     const res = await axios.get(
-  //       `http://localhost:3001/food/find/${props.state.query}`
-  //       // `${BASE_URL}?query=${state.query}&apiKey=${API_KEY}&number=5`
-  //     )
-  //     props.dispatch({ type: 'get_ingredients', payload: res.data })
-  //     console.log('hihihihi', props.state)
-  //   } catch (err) {
-  //     console.log(err)
-  //   }
-  // }
-
-  // const ingredientList = props.state.ingredients.length
-  //   ? props.state.ingredients.map((ingredient, index) => {
-  //       return (
-  //         <IngredientList
-  //           key={'recipe' + index}
-  //           // name={ingredient.name}
-  //           // img={ingredient.image}
-  //           ingredient={ingredient}
-  //           history={props.history}
-  //           dispatch={props.dispatch}
-  //           state={props.state}
-  //         />
-  //       )
-  //     })
-  //   : null
   return (
     <div>
       <Button id="dash-button" onClick={handleShow}>
@@ -77,91 +40,6 @@ const CreateRecipe = (props) => {
           />
         </Modal.Body>
       </Modal>
-      {/* <Modal show={show} dialogClassName="addFood">
-        <Modal.Header closeButton></Modal.Header>
-        <Modal.Body>
-          <form onSubmit={props.submitRecipe}>
-            <p>Image:</p>
-            <input
-              name="image"
-              value={props.newRecipe.image}
-              onChange={props.handleChange}
-            />
-            <p>Title:</p>
-            <input
-              name="name"
-              value={props.newRecipe.name}
-              onChange={props.handleChange}
-            />
-            <p>Cuisine:</p>
-            <input
-              name="cuisineType"
-              value={props.newRecipe.cuisineType}
-              onChange={props.handleChange}
-            />
-            <p>Instructions:</p>
-            <input
-              name="instructions"
-              value={props.newRecipe.instructions}
-              onChange={props.handleChange}
-            />
-            <p>Duration:</p>
-            <input
-              name="duration"
-              value={props.newRecipe.duration}
-              onChange={props.handleChange}
-            />
-            <p>Calories:</p>
-            <input
-              name="calories"
-              value={props.newRecipe.calories}
-              onChange={props.handleChange}
-            />
-            <p>Vegan:</p>
-            <input
-              type="checkbox"
-              value={props.isVegan}
-              onClick={props.handleVeganChange}
-            />
-            <p>DairyFree:</p>
-            <input
-              type="checkbox"
-              value={props.isDairyFree}
-              onClick={props.handleDairyChange}
-            />
-            <p>No Nuts:</p>
-            <input
-              type="checkbox"
-              value={props.hasNuts}
-              onClick={props.handleNutsChange}
-            />
-            <br></br>
-            <br></br>
-            <div id="addFood">
-              Ingredients:
-              <br></br>
-              <input
-                value={props.state.query}
-                onChange={(e) =>
-                  props.dispatch({ type: 'search', payload: e.target.value })
-                }
-              ></input>
-              <button onClick={() => getMyIngredients()}>search</button>
-              {ingredientList}
-              <br></br>
-            </div>
-            <div>
-              <Fridge
-                fridge={props.state.fridge}
-                removeIngredient={props.removeIngredient}
-              />
-            </div>
-            <button type="submit" onClick={handleClose}>
-              Create
-            </button>
-          </form>
-        </Modal.Body>
-      </Modal> */}
     </div>
   )
 }
--- client/src/components/Fridge.js
+++ client/src/components/Fridge.js
@@ -3,11 +3,9 @@ import React from 'react'
 const Fridge = (props) => {
   console.log(props)
 
-  const removeIngredient = () => {
-    props.dispatch({
-      type: 'remove_fridge',
-      payload: props.ingredient
-    })
+  const removeIngredient = (id) => {
+    let filtered = props.fridge.filter((ing) => ing.id !== id)
+    return props.dispatch({ type: 'remove_fridge', payload: filtered })
   }
 
   return (
@@ -16,7 +14,7 @@ const Fridge = (props) => {
         <div>
           <p>{fridgeItem.name}</p>
           <img src={fridgeItem.image} />
-          <button onClick={() => removeIngredient(props.ingredient)}>-</button>
+          <button onClick={() => removeIngredient()}>-</button>
         </div>
       ))}
     </div>
